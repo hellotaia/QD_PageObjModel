3b5426a8d80af28428e61b36d61adc14121f49f4
Taisiia Teslenko <taisiia.teslenko@redsailtechnologies.com>
2023-04-16 21:54:01 +0300

    IDoCanCommit

---
+++ QD_PageObjModel/MyAccountPage.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace QD_PageObjModel
+{
+    public class MyAccountPage
+    {
+       private IWebDriver driver;
+       private WebDriverWait wait;
+
+        public MyAccountPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+        }
+
+        //добавить локаторы на все основные элементы страницы (тайтлы, поля, кнопки);
+        //login column
+        public IWebElement LoginTitle => driver.FindElement(By.XPath("//div[@id='customer_login']/div/h2"));
+        public IWebElement LoginEmailField => driver.FindElement(By.XPath("//input[@id='username']"));
+        public IWebElement LoginPasswordField => driver.FindElement(By.XPath("//input[@id='password']"));
+        public IWebElement LoginButton => driver.FindElement(By.XPath("//input[@name='login']"));
+        public IWebElement LoginRememberCheckBox => driver.FindElement(By.XPath("//label[@for='rememberme']"));
+        public IWebElement LoginLostPswrd => driver.FindElement(By.XPath("//div[@id='customer_login']/div/form/p/a"));
+        // register column 
+        public IWebElement RegisterTitle => driver.FindElement(By.XPath("//div[contains(@class,'col-2')]/h2"));
+        public IWebElement RegisterEmailField => driver.FindElement(By.XPath("//input[@id='reg_email']"));
+        public IWebElement RegisterPasswordField => driver.FindElement(By.XPath("//input[@id='reg_password']"));
+        public IWebElement RegisterButton => driver.FindElement(By.XPath("//input[@value='Register']"));
+
+        //написать методы, возвращающие текстовые значения из: "Lost your password?", "Remember me" и кнопки "Register";
+        public string LostYourPswrdText()
+        {
+            return LoginLostPswrd.Text;
+        }
+        public string RememberMeText() 
+        {
+           return LoginRememberCheckBox.Text;
+        }
+        public string RegisterText()
+        {
+            string value = RegisterButton.GetAttribute("value");
+            return value; 
+        }
+
+        //написать метод который вводит переданные в данный метод значения в поля "Username or email address" и "Password" и после кликает по кнопке "Login";
+        public void LoginFlow(string username, string password)
+        {
+            LoginEmailField.SendKeys(username);
+            LoginPasswordField.SendKeys(password);
+            LoginButton.Click();
+        }
+    }
+}
--- QD_PageObjModel/MyAccountTests.cs
+++ QD_PageObjModel/MyAccountTests.cs
@@ -1,21 +1,15 @@
 ﻿using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
-using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using OpenQA.Selenium.Support.UI;
 
 namespace QD_PageObjModel
 {
-    public class MyAccTests
+    public class MyAccountTests
     {
         //создаем отдельный класс и реализуем автотесты для всех методов из пейдж класса с использованием ассертов NUnit.
         private IWebDriver driver;
         private WebDriverWait wait;
-        private MyAccPage myAccPage;
+        private MyAccountPage myAccPage;
 
         private string URL = "https://practice.automationtesting.in/my-account/";
 
@@ -24,7 +18,7 @@ namespace QD_PageObjModel
         {
             driver = new ChromeDriver();
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            myAccPage = new MyAccPage(driver);
+            myAccPage = new MyAccountPage(driver);
             driver.Navigate().GoToUrl(URL);
             driver.Manage().Window.Maximize();
         }
@@ -34,7 +28,8 @@ namespace QD_PageObjModel
         {
             string expectedText = "Lost your password?";
             string actualText = myAccPage.LostYourPswrdText();
-            Assert.AreEqual(expectedText, actualText, "LostPswrdLinkText values are not matched");
+            Assert.AreEqual(expectedText, actualText, 
+                "LostPswrdLinkText values are not matched");
         }
 
         [Test]
@@ -42,7 +37,8 @@ namespace QD_PageObjModel
         {
             string expextedText = "Remember me";
             string actualText = myAccPage.RememberMeText();
-            Assert.AreEqual(expextedText, actualText, "RememberMeText values are not matched");
+            Assert.AreEqual(expextedText, actualText, 
+                "RememberMeText values are not matched");
         }
 
         [Test]
@@ -50,7 +46,8 @@ namespace QD_PageObjModel
         {
             string expextedText = "Register";
             string actualText = myAccPage.RegisterText();
-            Assert.AreEqual(expextedText, actualText, "RegisterTextButton values are not matched");
+            Assert.AreEqual(expextedText, actualText, 
+                "RegisterTextButton values are not matched");
         }
         [Test]
         //реализовать тесты для логин флоу метода
@@ -58,8 +55,11 @@ namespace QD_PageObjModel
         {
             string username = "hhhhpepepellalfjf";
             string password = "qwerty";
+            string expectedError = $"Error: the username {username} is not registered on this site. " +
+                $"If you are unsure of your username, try your email address instead.";
             myAccPage.LoginFlow(username,password);
-            Assert.AreEqual($"Error: the username {username} is not registered on this site. If you are unsure of your username, try your email address instead.", driver.FindElement(By.XPath("//ul[@class=\"woocommerce-error\"]")).Text);
+            Assert.AreEqual(expectedError, 
+                driver.FindElement(By.XPath("//ul[@class=\"woocommerce-error\"]")).Text);
         }
 
         [Test]
@@ -68,7 +68,8 @@ namespace QD_PageObjModel
             string username = "mmm@cc.com";
             string password = "qwerty";
             myAccPage.LoginFlow(username, password);
-            Assert.AreEqual("Error: A user could not be found with this email address.", driver.FindElement(By.XPath("//ul[@class=\"woocommerce-error\"]")).Text);
+            Assert.AreEqual("Error: A user could not be found with this email address.", 
+                driver.FindElement(By.XPath("//ul[@class=\"woocommerce-error\"]")).Text);
         }
         
         [Test]
@@ -79,8 +80,23 @@ namespace QD_PageObjModel
             string password = "Password12345!@#$%";
             myAccPage.LoginFlow(email, password);          
             Assert.AreEqual(username, driver.FindElement(By.XPath("//*[@class=\"type-page\"]/div/div[1]/div/p[1]/strong")).Text);
+        }
 
-
+        [Test]
+        public void TestLoginWithInvalidPassword()
+        {
+            string email = "testuser@test.com";
+            string username = "testuser7";
+            string password = "qwerty";
+            string expectedErrorEmail = $"Error: the password you entered for the username {email} is incorrect. Lost your password?";
+            string expectedErrorUser = $"Error: the password you entered for the username {username} is incorrect. Lost your password?";
+            myAccPage.LoginFlow(email, password);
+            Assert.AreEqual(expectedErrorEmail,
+                driver.FindElement(By.XPath("//ul[@class=\"woocommerce-error\"]")).Text);
+            myAccPage.LoginEmailField.Clear();
+            myAccPage.LoginFlow(username, password);
+            Assert.AreEqual(expectedErrorUser,
+                driver.FindElement(By.XPath("//ul[@class=\"woocommerce-error\"]")).Text);
         }
         [TearDown]
         public void TearDown()
